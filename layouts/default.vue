9f8f3cb2dc3649d01357a8fced091d1d93d9b1e5
Szilvási István Péter <szilvasipeter@protonmail.com>
2026-06-07 16:55:16 +0200

    feat(layout) - add default layout template for the application

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="slidev-layout default w-full h-full p-0 flex flex-col bg-white">
+    <header
+      class="w-full bg-[#1a2a5a] h-28 flex items-center justify-between px-12 border-b-[6px] border-[#c8a048] relative z-20 shadow-md"
+    >
+      <div class="text-white text-4xl font-bold font-sans">
+        <slot name="header" />
+      </div>
+
+      <div class="flex flex-col items-center">
+        <img src="/assets/header.png" class="h-15 filter" />
+      </div>
+    </header>
+
+    <main class="flex-1 w-full relative p-12 overflow-hidden">
+      <div
+        class="absolute inset-0 opacity-40 flex justify-center items-center pointer-events-none"
+      >
+        <img
+          src="/assets/ekke.png"
+          class="w-2/3 filter opacity-30 object-contain"
+        />
+      </div>
+
+      <div class="relative z-10 text-[#1a2a5a]">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
